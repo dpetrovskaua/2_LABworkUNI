0b4c779f503c1afb5fc38152824e05a7d87a4fe9
DianaPetrovska <259563350+dpetrovskaua@users.noreply.github.com>
2026-04-02 00:16:12 +0300

    Testing of main.py

---
+++ testing.py
@@ -0,0 +1,22 @@
+import unittest
+from main2 import TextProcessor
+
+class TestTextProcessor(unittest.TestCase):
+    def setUp(self):
+        self.processor = TextProcessor()
+
+    def test_process_basic(self):
+        self.assertEqual(self.processor.process_1word("транзистор"), "транзисор")
+        self.assertEqual(self.processor.process_1word("максимум"), "максиу")
+        self.assertEqual(self.processor.process_1word("minimum"), "miniu")
+
+    def test_process_capital(self): # з великими буквами
+        self.assertEqual(self.processor.process_1word("Ананас"), "Аннс")
+        self.assertEqual(self.processor.process_1word("Елемент"), "Елмнт")
+
+    def test_process_small(self): # малі слова чи окремі букви
+        self.assertEqual(self.processor.process_1word("я"), "я")
+        self.assertEqual(self.processor.process_1word("її"), "ї")
+
+if __name__ == "__main__":
+    unittest.main()
