392dd625f789c4058bd23ba9ec8da996e7331735
DianaPetrovska <259563350+dpetrovskaua@users.noreply.github.com>
2026-04-02 18:45:31 +0300

    Testing of main.py

--- testing.py
+++ testing.py
@@ -9,14 +9,18 @@ class TestTextProcessor(unittest.TestCase):
         self.assertEqual(self.processor.process_1word("транзистор"), "транзисор")
         self.assertEqual(self.processor.process_1word("максимум"), "максиу")
         self.assertEqual(self.processor.process_1word("minimum"), "miniu")
-
+        self.assertEqual(self.processor.process_1word("pineapple"), "pineale")
+    
     def test_process_capital(self): # з великими буквами
         self.assertEqual(self.processor.process_1word("Ананас"), "Аннс")
         self.assertEqual(self.processor.process_1word("Елемент"), "Елмнт")
+        self.assertEqual(self.processor.process_1word("People"), "Peole")
+        self.assertEqual(self.processor.process_1word("Calculator"), "Calulator")
 
     def test_process_small(self): # малі слова чи окремі букви
         self.assertEqual(self.processor.process_1word("я"), "я")
         self.assertEqual(self.processor.process_1word("її"), "ї")
-
+         self.assertEqual(self.processor.process_1word("A"), "A")
+        
 if __name__ == "__main__":
     unittest.main()
